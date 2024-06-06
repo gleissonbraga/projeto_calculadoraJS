6bb8532b142b484578f039915f6002ad3866f347
Gleisson Braga <135471754+Gleissonbm@users.noreply.github.com>
2024-06-06 07:18:36 -0300

    adicionando opção de tema escuro ou claro

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const main = document.querySelector('main')
-const root = document.querySelector('root')
+const root = document.querySelector(':root')
 const input = document.getElementById('input')
 const resultInput = document.getElementById('result')
 
@@ -46,3 +46,22 @@ function calculate() {
     const result = eval(input.value)
     resultInput.value = result
 }
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+    if (main.dataset.theme === "dark") {
+      root.style.setProperty("--bg-color", "#f1f5f9")
+      root.style.setProperty("--border-color", "#aaa")
+      root.style.setProperty("--font-color", "#212529")
+      root.style.setProperty("--primary-color", "#26834a")
+      main.dataset.theme = "light"
+    } else {
+      root.style.setProperty("--bg-color", "#212529")
+      root.style.setProperty("--border-color", "#666")
+      root.style.setProperty("--font-color", "#f1f5f9")
+      root.style.setProperty("--primary-color", "#4dff91")
+      main.dataset.theme = "dark"
+    }
+  })
+
+
+  // parei no video 21 no minuto 6:52
